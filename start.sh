6f424b3042e604be474584e3a2a42eb870b1ceae
dilin993 <you@example.com>
2020-05-23 12:33:16 +0300

    correct start/stop scripts

--- start.sh
+++ start.sh
@@ -11,5 +11,14 @@ fi
 
 
 
-#FLASK_APP=main.py
+export FLASK_APP=main.py
 #python -m flask run
+if [[ ! -e /tmp/rpi-home.pid ]]; then   # Check if the file already exists
+    python -m flask run --host=0.0.0.0 &   #+and if so do not run another process.
+    echo $! > /tmp/rpi-home.pid
+else
+    echo -n "ERROR: The process is already running with pid "
+    cat /tmp/rpi-home.pid
+    echo
+fi
+
--- stop.sh
+++ stop.sh
@@ -6,3 +6,12 @@ if [[ -e /tmp/read_sensors.py.pid ]]; then   # If the file do not exists, then t
 else
     echo "read_sensors.py is not running"
 fi
+
+
+if [[ -e /tmp/rpi-home.pid ]]; then   # If the file do not exists, then the
+    kill `cat /tmp/rpi-home.pid`      #+the process is not running. Useless
+    rm /tmp/rpi-home.pid              #+trying to kill it.
+else
+    echo "read_sensors.py is not running"
+fi
+
